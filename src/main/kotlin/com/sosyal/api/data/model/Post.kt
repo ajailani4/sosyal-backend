5d85635ff1ee6d829e6d64786d98e3667f69c8d4
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-03-17 22:09:14 +0700

    Update Post model

--- src/main/kotlin/com/sosyal/api/data/model/Post.kt
+++ src/main/kotlin/com/sosyal/api/data/model/Post.kt
@@ -1,12 +1,15 @@
 package com.sosyal.api.data.model
 
+import kotlinx.serialization.Serializable
 import org.bson.codecs.pojo.annotations.BsonId
 import org.litote.kmongo.Id
 
+@Serializable
 data class Post(
     @BsonId
     val id: Id<Post>? = null,
+    val username: String,
     val content: String,
+    val like: String,
     val date: String,
-    val username: String
 )
