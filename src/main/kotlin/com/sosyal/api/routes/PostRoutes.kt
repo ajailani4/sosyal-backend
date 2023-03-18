f329b1191db04e7e7a3bdd91b30664601754767e
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-03-18 20:42:32 +0700

    Add deletePost function in PostRoutes

--- src/main/kotlin/com/sosyal/api/routes/PostRoutes.kt
+++ src/main/kotlin/com/sosyal/api/routes/PostRoutes.kt
@@ -1,16 +1,21 @@
 package com.sosyal.api.routes
 
 import com.sosyal.api.data.dto.PostDto
+import com.sosyal.api.data.dto.response.BaseResponse
 import com.sosyal.api.data.repository.PostRepository
 import com.sosyal.api.util.Connection
+import io.ktor.http.*
+import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
 import org.koin.ktor.ext.inject
 
 fun Route.configurePostRoutes(connections: MutableSet<Connection>) {
@@ -57,6 +62,28 @@ fun Route.configurePostRoutes(connections: MutableSet<Connection>) {
             } catch (e: Exception) {
                 println(e.localizedMessage)
             }
+        }
+
+        delete("/posts/{id?}") {
+            val id = call.parameters["id"].toString()
+
+            val result = postRepository.deletePost(id)
+
+            if (!result) {
+                return@delete call.respond(
+                    status = HttpStatusCode.BadRequest,
+                    message = BaseResponse<JsonObject>(
+                        message = "Cannot delete the post"
+                    )
+                )
+            }
+
+            call.respond(
+                status = HttpStatusCode.OK,
+                message = BaseResponse<JsonObject>(
+                    message = "Post has been deleted successfully"
+                )
+            )
         }
     }
 }
