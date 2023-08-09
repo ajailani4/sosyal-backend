40683bb25df83dba53d9e82514e8eecf4731b798
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-08-09 16:20:21 +0700

    Add get chats by username route

--- src/main/kotlin/com/sosyal/api/routes/ChatRoutes.kt
+++ src/main/kotlin/com/sosyal/api/routes/ChatRoutes.kt
@@ -6,6 +6,7 @@ import com.sosyal.api.data.repository.ChatRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -17,6 +18,20 @@ fun Route.configureChatRoutes() {
 
     authenticate("auth-jwt") {
         route("/chats") {
+            get {
+                val principal = call.principal<JWTPrincipal>()
+                val username = principal!!.payload.getClaim("username").asString()
+                val chatsDto = chatRepository.getChatsByParticipants(username)
+
+                call.respond(
+                    status = HttpStatusCode.OK,
+                    message = BaseResponse(
+                        message = "Chats have been retrieved successfully",
+                        data = chatsDto
+                    )
+                )
+            }
+
             post {
                 val chatDto = call.receive<ChatDto>()
                 val isSuccess = chatRepository.createChat(chatDto)
@@ -33,7 +48,7 @@ fun Route.configureChatRoutes() {
                 call.respond(
                     status = HttpStatusCode.OK,
                     message = BaseResponse<JsonObject>(
-                        message = "Post has been deleted successfully"
+                        message = "Chat has been created successfully"
                     )
                 )
             }
