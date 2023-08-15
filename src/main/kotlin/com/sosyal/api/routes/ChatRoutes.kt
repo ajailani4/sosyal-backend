a0421bce8164f67c8573c5e5f8226039da1e2317
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-08-15 12:33:23 +0700

    Add real-time message on chat route

--- src/main/kotlin/com/sosyal/api/routes/ChatRoutes.kt
+++ src/main/kotlin/com/sosyal/api/routes/ChatRoutes.kt
@@ -1,8 +1,10 @@
 package com.sosyal.api.routes
 
 import com.sosyal.api.data.dto.ChatDto
+import com.sosyal.api.data.dto.MessageDto
 import com.sosyal.api.data.dto.response.BaseResponse
 import com.sosyal.api.data.repository.ChatRepository
+import com.sosyal.api.util.ChatConnection
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
@@ -10,13 +12,66 @@ import io.ktor.server.auth.jwt.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import org.koin.ktor.ext.inject
 
-fun Route.configureChatRoutes() {
+fun Route.configureChatRoutes(chatConnections: MutableSet<ChatConnection>) {
     val chatRepository by inject<ChatRepository>()
 
     authenticate("auth-jwt") {
+        webSocket("/chat") {
+            val principal = call.principal<JWTPrincipal>()
+            val username = principal!!.payload.getClaim("username").asString()
+            val chatId = call.request.queryParameters["chatId"].toString()
+            val chatConnection = ChatConnection(
+                session = this,
+                chatId = chatId,
+                username = username
+            )
+            chatConnections += chatConnection
+
+            try {
+                val messages = chatRepository.getMessagesByChatId(chatId)
+
+                if (messages.isNotEmpty()) {
+                    messages.forEach { messageDto ->
+                        chatConnection.session.send(Json.encodeToString(messageDto))
+                    }
+                } else {
+                    chatConnection.session.send(
+                        Json.encodeToString(
+                            MessageDto(
+                                chatId = "",
+                                username = "",
+                                content = ""
+                            )
+                        )
+                    )
+                }
+
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val messageDtoText = frame.readText()
+                    val messageDto = Json.decodeFromString<MessageDto>(messageDtoText)
+
+                    val id = chatRepository.addMessage(messageDto)
+
+                    chatConnections.filter { it.chatId == chatId }.forEach { conn ->
+                        conn.session.send(Json.encodeToString(messageDto.copy(id = id)))
+                    }
+                }
+            } catch (e: Exception) {
+                println(e.localizedMessage)
+            } finally {
+                println("Remove $chatConnection")
+                chatConnections -= chatConnection
+            }
+        }
+
         route("/chats") {
             get {
                 val principal = call.principal<JWTPrincipal>()
